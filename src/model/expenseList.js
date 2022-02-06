158a6bd4c346784ad05829927366efcb7d576954
Yuriy Koziy <yukoziy@gmail.com>
2022-02-05 19:16:29 -0500

    Added expenseList with the related tests.

---
+++ src/model/expenseList.js
@@ -0,0 +1,41 @@
+class ExnepseList {
+  constructor() {
+    this.expenses = {};
+  }
+
+  add(expense, category = 'other') {
+    if (!this.expenses.hasOwnProperty(category)) {
+      this.expenses[category] = [expense];
+    } else {
+      this.expenses[category].push(expense);
+    }
+  }
+
+  getExpenses() {
+    return this.expenses;
+  }
+
+  sumCategory(category) {
+    if (this.expenses.hasOwnProperty(category)) {
+      const expensesArray = this.expenses[category];
+      let sum = 0;
+      expensesArray.forEach((expense) => {
+        sum += expense.cost;
+      });
+      return sum;
+    } else {
+      return 0;
+    }
+  }
+
+  deleteCategory(category) {
+    if (this.expenses.hasOwnProperty(category)) {
+      delete this.expenses[category];
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+export default ExnepseList;
---
+++ test/expenseList.test.js
@@ -0,0 +1,66 @@
+import Expense from '../src/model/expense';
+import ExpenseList from '../src/model/expenseList';
+
+describe('Testing ExpenseList class', () => {
+  it('should allow to add multiple expenses without category, adding two. Will be in other category by default', () => {
+    const expenseA = new Expense('gas', 35.78);
+    const expenseB = new Expense('food', 560.1);
+
+    const expenses = new ExpenseList();
+    expenses.add(expenseA);
+    expenses.add(expenseB);
+
+    expect(expenses.getExpenses()['other'].length).toEqual(2);
+  });
+
+  it('sum of non existing category should be 0', () => {
+    const expenseA = new Expense('gas', 35.78);
+    const expenseB = new Expense('food', 560.1);
+
+    const expenses = new ExpenseList();
+
+    expect(expenses.sumCategory('fun')).toEqual(0);
+  });
+
+  it('should return the sum of all expenses in category. Other (two expenses of total 595.88 ), fun (one expense of total 250.45)', () => {
+    const expenseA = new Expense('gas', 35.78);
+    const expenseB = new Expense('food', 560.1);
+
+    const expenseC = new Expense('ski lessons', 250.45);
+
+    const expenses = new ExpenseList();
+    expenses.add(expenseA);
+    expenses.add(expenseB);
+    expenses.add(expenseC, 'fun');
+
+    expect(expenses.getExpenses()['other'].length).toEqual(2);
+    expect(expenses.getExpenses()['fun'].length).toEqual(1);
+    expect(expenses.sumCategory('other')).toEqual(595.88);
+    expect(expenses.sumCategory('fun')).toEqual(250.45);
+  });
+
+  it('deleting non existing category should return false', () => {
+    const expenseA = new Expense('gas', 35.78);
+    const expenseB = new Expense('food', 560.1);
+
+    const expenses = new ExpenseList();
+    expenses.add(expenseA);
+    expenses.add(expenseB);
+
+    expect(expenses.deleteCategory('fun')).toBe(false);
+  });
+
+  it('should delete existing category', () => {
+    const expenseA = new Expense('gas', 35.78);
+    const expenseB = new Expense('food', 560.1);
+    const expenseC = new Expense('ski lessons', 250.45);
+
+    const expenses = new ExpenseList();
+    expenses.add(expenseA);
+    expenses.add(expenseB);
+    expenses.add(expenseC, 'fun');
+
+    expect(expenses.deleteCategory('other')).toBe(true);
+    expect(Object.keys(expenses.getExpenses()).length).toEqual(1);
+  });
+});
