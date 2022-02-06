9c0b360edd441e52c28a64f7ecbfd59a3af8978b
Yuriy Koziy <yukoziy@gmail.com>
2022-02-05 19:22:29 -0500

    Added Income and related tests

---
+++ src/model/income.js
@@ -0,0 +1,8 @@
+class Income {
+  constructor(title, income) {
+    this.title = title;
+    this.income = income;
+  }
+}
+
+export default Income;
---
+++ test/income.test.js
@@ -0,0 +1,9 @@
+import Income from '../src/model/income';
+
+describe('Testing Income class', () => {
+  it('should set title and income with new Income("full-time job", 32456)', () => {
+    const income = new Income('full-time job', 32456);
+    expect(income.title).toEqual('full-time job');
+    expect(income.income).toEqual(32456);
+  });
+});
