9ec034e211a1cc159bde4ef98ec2b108af8b7923
Yuriy Koziy <yukoziy@gmail.com>
2022-04-10 09:44:03 -0400

    Fixed displayChart, so that only one svg element exists

--- src/view/view.js
+++ src/view/view.js
@@ -220,6 +220,10 @@ class View {
 
   displayChart(expenses, incomes) {
     const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    let svgElement = document.querySelector('svg');
+    if (svgElement) {
+      svgElement.remove();
+    }
     this.chartPanel.append(svg);
     const chart = new PieChart(this.chartPanel, expenses);
     chart.show();
