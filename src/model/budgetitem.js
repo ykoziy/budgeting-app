2aff6d6f7d38bb9b1ca5afb0643eb91cc78747ff
Yuriy Koziy <yukoziy@gmail.com>
2022-04-28 16:50:46 -0400

    Added uniqid to the budget item

--- src/model/budgetitem.js
+++ src/model/budgetitem.js
@@ -1,7 +1,14 @@
+import uniqid from 'uniqid';
+
 class BudgetItem {
   constructor(title, money) {
+    this.id = uniqid();
     this.title = title;
     this.money = money;
+  }
+
+  getId() {
+    return this.id;
   }
 }
 
